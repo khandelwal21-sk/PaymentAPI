c1c9ff534b5eff91a6ef40e2868a0878b3437adb
shubham <shubham.gupta@easemytrip.com>
2024-12-18 13:10:37 +0530

    Add project files.

---
+++ Models/PaymentDetails.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPi.Models
+{
+    public class PaymentDetails
+    {
+        [Key]
+        public int PaymentDetailID { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber  { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
